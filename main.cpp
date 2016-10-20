f0b4e50e7a4c2ab3a2cfb823315c15734735b77a
jnasante <josephnasante@yahoo.com>
2016-10-20 11:32:02 -0500

    Added conversion from hex to binary before calculating k (for more precise k)

--- main.cpp
+++ main.cpp
@@ -31,17 +31,47 @@ string getRandomWithPrefix() {
     return addPrefix(randomString);
 }
 
+// This method was taken from here: http://www.cplusplus.com/forum/beginner/41657/
+string convertHexToBinary (string sHex)
+{
+    string sReturn = "";
+    for (int i = 0; i < sHex.length (); ++i)
+    {
+        switch (sHex [i])
+        {
+            case '0': sReturn.append ("0000"); break;
+            case '1': sReturn.append ("0001"); break;
+            case '2': sReturn.append ("0010"); break;
+            case '3': sReturn.append ("0011"); break;
+            case '4': sReturn.append ("0100"); break;
+            case '5': sReturn.append ("0101"); break;
+            case '6': sReturn.append ("0110"); break;
+            case '7': sReturn.append ("0111"); break;
+            case '8': sReturn.append ("1000"); break;
+            case '9': sReturn.append ("1001"); break;
+            case 'a': sReturn.append ("1010"); break;
+            case 'b': sReturn.append ("1011"); break;
+            case 'c': sReturn.append ("1100"); break;
+            case 'd': sReturn.append ("1101"); break;
+            case 'e': sReturn.append ("1110"); break;
+            case 'f': sReturn.append ("1111"); break;
+        }
+    }
+    return sReturn;
+}
+
 int getK(string values[]) {
     int k = 0;
-    while (k < values[0].length()) {
-        if (values[0][k] != values[1][k]) {
+    string bitValues[] = { convertHexToBinary(values[0]), convertHexToBinary(values[1]) };
+    while (k < bitValues[0].length()) {
+        if (bitValues[0][k] != bitValues[1][k]) {
             break;
         }
         
         k++;
     }
     
-    return k * 4;
+    return k;
 }
 
 void printResults(int k) {
